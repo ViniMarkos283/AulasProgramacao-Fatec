6756d17f6554fc3dc516c4b052aac7f8e010b7d7
Marcos Vinicius de Oliveira <102962949+ViniMarkos283@users.noreply.github.com>
2024-03-21 22:46:30 -0300

    Update ConversorDolar.java

--- Tecnicas-de-programação/aula-15-03-24/atividades/src/ConversorDolar.java
+++ Tecnicas-de-programação/aula-15-03-24/atividades/src/ConversorDolar.java
@@ -11,14 +11,14 @@ import java.util.Scanner;
 public class ConversorDolar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double real, dollar;
+        double cot, dollar;
 
-        System.out.println("Qual a cotação atual do dolar?");
+        System.out.println("Qual a cotação atual do dolar? ");
+        cot = Double.parseDouble(sc.nextLine());
+
+        System.out.println("Quantos dolares voce tem disponivel: ");
         dollar = Double.parseDouble(sc.nextLine());
 
-        System.out.println("Qual o valor em Reais que voce quer converter para dolar:");
-        real = Double.parseDouble(sc.nextLine());
-
-        System.out.printf("O valor %.2f em reais equivale a %.2f dolares", real, real / dollar);
+        System.out.printf("O valor %.2f em dolares equivale a %.2f reais", dollar, dollar * cot);
     }
 }
