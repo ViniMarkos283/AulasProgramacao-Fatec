1faa534a8b7ea84ea4b9bfc12bff47f5974f8f0f
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-04-16 09:21:24 -0300

    nothing

--- Estrutura-de-dados/aula-24-04-15/ListaDeInteiros/src/App.java
+++ Estrutura-de-dados/aula-24-04-15/ListaDeInteiros/src/App.java
@@ -1,3 +1,12 @@
+// aula do dia 15/04 e tbm do dia 16/04
+
+/*
+ * resumão do for
+ * 1. indice a partir de onde o processo inicia (i = 1)
+ * 2. critério de continuidade do processo i >= 0
+ * 3. atualização da variavel (i = i + 1)
+*/
+
 import java.util.Scanner;
 
 public class App {
@@ -11,44 +20,53 @@ public class App {
                 "\n5 - adiciona posição especifica\n6 - remove posição especifica" +
                 "\n7 - Mostrar elementos da lista\n8 - Sair";
 
-        do { // roda o loop pelo menos uma vez sem checar a condiçao
-            System.out.println(menu);
-            op = sc.nextInt();
+        try {
+            do { // roda o loop pelo menos uma vez sem checar a condiçao
+                System.out.println(menu);
+                op = sc.nextInt();
 
-            switch (op) {
-                case 1:
-                    System.out.println("Digite um valor: ");
-                    valor = sc.nextInt();
-                    li.adicionaFinal(valor);
-                    break;
-                case 2:
-                    System.out.println("Remover Final");
-                    break;
-                case 3:
-                    System.out.println("Adicionar Inicio");
-                    break;
-                case 4:
-                    System.out.println("Remover Inicio");
-                    break;
-                case 5:
-                    System.out.println("Adicionar posição especifica");
-                    break;
-                case 6:
-                    System.out.println("Remover posição especifica");
-                    break;
-                case 7:
-                    System.out.println("Mostrando todos os elementos da lista");
-                    System.out.println("Lista: " + li.toString());
-                    break;
-                case 8:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("\tOpção Inválida, tente novamente\t");
-                    break;
-            }
-        } while (op != 8);
+                switch (op) {
+                    case 1:
+                        System.out.println("Digite um valor: ");
+                        valor = sc.nextInt();
+                        li.adicionaFinal(valor);
+                        break;
+                    case 2:
+                        valor = li.removeFinal();
+                        System.out.println("Elemento removido: " + valor);
+                        break;
+                    case 3:
+                        System.out.println("Digite um valor: ");
+                        valor = sc.nextInt();
+                        li.adicionaInicio(valor);
+                        break;
+                    case 4:
+                        valor = li.removeInicio();
+                        System.out.println("Elemento removido: " + valor);
+                        break;
+                    case 5:
+                        System.out.println("Adicionar posição especifica");
+                        break;
+                    case 6:
+                        System.out.println("Remover posição especifica");
+                        break;
+                    case 7:
+                        System.out.println("Mostrando todos os elementos da lista");
+                        System.out.println("Lista: " + li.toString());
+                        break;
+                    case 8:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("\tOpção Inválida, tente novamente\t");
+                        break;
+                }
+            } while (op != 8);
+
+        } catch (Exception e) { // tratamento de erros
+            System.out.println(e.getMessage());
+        }
 
         sc.close();
     }
-}
+} // fazer as outras em casa...
--- Estrutura-de-dados/aula-24-04-15/ListaDeInteiros/src/ListaDeInteiros.java
+++ Estrutura-de-dados/aula-24-04-15/ListaDeInteiros/src/ListaDeInteiros.java
@@ -2,27 +2,61 @@ public class ListaDeInteiros {
     private int dados[];
     private int tamanho;
 
-    public ListaDeInteiros(int capMax){
-    dados = new int[capMax];
-    tamanho = 0;
-}
+    public ListaDeInteiros(int capMax) {
+        dados = new int[capMax];
+        tamanho = 0;
+    }
 
-public void adicionaFinal(int e){
-    if (dados.length != tamanho ) { // se n estiver cheia...
-        dados[tamanho] = e;
-        tamanho += 1;
+    public void adicionaFinal(int e) {
+        if (dados.length != tamanho) { // se n estiver cheia...
+            dados[tamanho] = e;
+            tamanho += 1;
+        } else { // se estiver cheia...
+            System.out.println("Erro!, lista cheia!");
+        }
     }
-    else{ // se estiver cheia...
-        System.out.println("Erro!, lista cheia!");
-    }
-}
 
-public String toString(){ // ???
-    String r = ""; // inicializa um str vazia | tamanho 0
-    for(int i = 0; i < tamanho; i++){
-        r += dados[i] + "\t"; // \t é um TAB
+    public int removeFinal() throws Exception { // remove o ultimo valor da lista
+        int r = 1;
+        if (tamanho != 0) { // se for diferente de zero...
+            r = dados[tamanho - 1];
+            tamanho = tamanho - 1;
+        } else { // se o tamanho da lista for = a zero...
+            throw new Exception("Erro!, Lista vazia!"); // dispara uma msg de erro com a mensagem escolhida
+        }
+        return r;
     }
-    return r;
-}
+
+    public void adicionaInicio(int e) throws Exception {
+        if (tamanho == dados.length) {
+            throw new Exception("Erro! a lista está cheia!");
+        } else {
+            for (int i = tamanho - 1; i >= 0; i--) { // inicializa o indice [i] pelo -1
+                dados[i + 1] = dados[i];
+            }
+            dados[0] = e;
+            tamanho++;
+        }
+    }
+
+    public int removeInicio() throws Exception {
+        int r = 1;
+        if (tamanho != 0) {
+            r = dados[tamanho + 1];
+            tamanho -= 1; // terminar dps
+        } else {
+            throw new Exception("Erro! lista vazia!");
+        }
+
+        return r;
+    }
+
+    public String toString() { // retorna todos os valores de dentro da lista
+        String r = ""; // inicializa um str vazia | tamanho 0
+        for (int i = 0; i < tamanho; i++) {
+            r += dados[i] + "\t"; // \t é um TAB
+        }
+        return r;
+    }
 
 }
