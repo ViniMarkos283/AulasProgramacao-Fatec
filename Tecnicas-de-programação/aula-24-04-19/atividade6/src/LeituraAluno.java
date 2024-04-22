0e98334ce2481a3fb63fc5e09953d88d7afd146d
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-04-22 11:50:35 -0300

    new questions

---
+++ Tecnicas-de-programação/aula-24-04-19/atividade6/src/LeituraAluno.java
@@ -0,0 +1,32 @@
+//Nome: Marcos Vinicius de Oliveira
+//RA: 2171392321015
+// Objetivo:
+/* Implemente um código possível para a classe da figura abaixo.
+    Aluno
+    - nome: String
+    matricula: String
+    nota1: double
+    nota2: double
+    notaReavaliacao: double
+    + Aluno()
+    + Aluno(String nome, double matricula, double nota1, double nota2, double
+    notaReavaliacao) + toString(): String
+    + calcularMedia(): double
+    + calcularMediaFinal(): double
+    a. calcularMedia()
+    Deve calcular a média das notas (nota1 + nota2)/2
+    Retornar valor double
+    b. calcularMediaFinal()
+    Se calcularMedia() >= 6
+    • retornar calcularMedia()
+    Senão
+    • retornar (calcularMedia() + notaReavaliacao)/2
+    C. Crie uma classe para testar a classe Aluno
+*/
+
+
+public class LeituraAluno {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Tecnicas-de-programação/aula-24-04-19/atividade6/src/LeituraContaCorrente.java
@@ -0,0 +1,33 @@
+//Nome: Marcos Vinicius de Oliveira
+//RA: 2171392321015
+// Objetivo:
+/* 67. Criar uma classe para representar uma entidade que representa uma
+    Conta Corrente em um sistema bancário. A conta corrente deve ter os
+    seguintes atributos (todos privados):
+    • Nome do Correntista
+    • Sexo
+    • Idade
+    • Numero da Conta
+    • Flag indicando se é conta conjunta
+    • Saldo
+    Todos os atributos devem ter seus respectivos métodos Getters e Setters.
+    Criar os seguintes métodos na classe Conta:
+    • Saque: Recebe como parâmetro o valor. Verifica o saldo da
+    conta, e se tiver saldo disponível, diminui do saldo o valor
+    sacado.
+    • Saldo: não recebe parâmetros. Deve imprimir um cabeçalho
+    com o nome do correntista, numero da conta, indicação se é
+    conta conjunta ou não, e o saldo em conta.
+    • Deposito: Recebe como parâmetro o valor depositado na
+    conta.
+    • Aumenta o saldo da conta de acordo com o valor depositado.
+    • Criar uma classe chamada TestaConta, que deve possuir um método
+    main.
+    No método main devem ser instanciados 3 objetos do tipo ContaCorrente,
+    com os seguintes valores
+*/
+
+public class LeituraContaCorrente {
+    public static void main(String[] args) throws Exception {
+    }
+}
