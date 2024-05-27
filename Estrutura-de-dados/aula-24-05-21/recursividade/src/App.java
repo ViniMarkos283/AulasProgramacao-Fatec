1bd35af165c98a0b8dc94f270098d1a12605446f
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-05-27 09:18:54 -0300

    day 27/05/24

--- Estrutura-de-dados/aula-24-05-21/recursividade/src/App.java
+++ Estrutura-de-dados/aula-24-05-21/recursividade/src/App.java
@@ -1,10 +1,39 @@
+// aulas do dia 21/05 e 27/05
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int vet[] = {2,-4, 7, 0, -1, 4};
+        // int vet[] = {2,-4, 7, 0, -1, 4};
+        int n1, n2, n3;
 
-        System.out.println(Metodos.metodoB(7, 2)); // metodo da somatória
-        System.out.println(Metodos.metodoF(vet, 6)); // metodo recursivo F, soma somente maiores q zero
-        System.out.println(Metodos.fibonacci(5)); // colocar valores altos quebra esse metodo, pq chama muitas vezes ele mesmo
+        Metodos met = new Metodos();
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("primeiro numero: ");
+        n1 = sc.nextInt();
+        System.out.println("Segundo numero: ");
+        n2 = sc.nextInt();
+        
+        // System.out.println(met.metodoB(7, 2)); // metodo da somatória
+        // System.out.println(met.metodoF(vet, 6)); // metodo recursivo F, soma somente maiores q zero
+        // System.out.println(met.fibonacci(5)); // colocar valores altos quebra esse metodo, pq chama muitas vezes ele mesmo
+        if (n1>n2){
+            System.out.println("MDC = " + met.mdc(n1, n2)); // faz o mdc de dois valores, encontrando seu menor divisor em comum
+        }else{
+            System.out.println("MDC = " + met.mdc(n2, n1)); 
+        }
+
+        System.out.println("Qual termo de fibonacci você quer? ");
+        n3 = sc.nextInt();
+        int vetor[] = new int[n3];
+
+        met.fibo(n3, vetor);
+        System.out.println("elemento = " + vetor[n3-1]);
+
+
+        sc.close();
     }
 }
- // em casa, fazer o exe 4
+ // em casa, fazer o exe 4 [ esqueci ]
--- Estrutura-de-dados/aula-24-05-21/recursividade/src/Metodos.java
+++ Estrutura-de-dados/aula-24-05-21/recursividade/src/Metodos.java
@@ -1,5 +1,5 @@
 public class Metodos {
-    public static int metodoB(int n, int m) { // faz a somatoria dos numeros no intervalo de M até o N
+    public int metodoB(int n, int m) { // faz a somatoria dos numeros no intervalo de M até o N
         if (m > n) { // se o M for maior, zera
             return 0;
         }
@@ -10,7 +10,7 @@ public class Metodos {
                                                   // uma das codições acima seja atendida, depois soma tudo e devolve
     }
 
-    public static int metodoF(int v[], int n) {
+    public int metodoF(int v[], int n) {
         if (n == 0) {
             return 0;
         }
@@ -21,13 +21,35 @@ public class Metodos {
         return s;
     }
 
-    public static int fibonacci(int n){
+    public int fibonacci(int n) { // metodo fibonacci
         if (n == 0) {
             return 0;
         }
         if (n == 1) {
             return 1;
         }
-        return fibonacci(n-1) + fibonacci(n-2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public int mdc(int m, int n) { // minimo divisor comum...
+        int r = m % n; // resto da divisão
+        if (r != 0) {
+            return mdc(n, r); // retorna o metodo com os valores alterados até q o resto seja zero
+        }
+        return n; // retorna o divisor
+
+    }
+
+    public void fibo(int n, int v[]) { // outro jeito de fazer o metodo do fibonacci (acho o primeiro mais facil)
+        if (n == 1) {
+            v[n - 1] = 1; // se o N for 1...
+        } else {
+            fibo(n - 1, v);
+            if (n == 2) { // se N for 2...
+                v[n - 1] = 1;
+            } else { // senão, faz a conta normalmente
+                v[n - 1] = v[n - 3] + v[n - 2];
+            }
+        }
     }
 }
