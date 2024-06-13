977d619c8f769fb22064ba8d841434e9143fc811
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-06-13 12:41:23 -0300

    Update App.java

--- Estrutura-de-dados/aula-24-06-13/Grafos-e-matrizes/src/App.java
+++ Estrutura-de-dados/aula-24-06-13/Grafos-e-matrizes/src/App.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Matriz m = new Matriz(4, 4);
+        Scanner sc = new Scanner(System.in);
+        int linhas, colunas, opcao, l, c;
 
-        m.adiciona(0, 1);// marca uma localização na matriz, que receberá 1
-        m.adiciona(0, 2);
-        m.adiciona(0, 3);
-        m.adiciona(1, 0);
+        System.out.println("Quantas vertices seu grafo tem?");
+        linhas = sc.nextInt();
+        colunas = linhas;
+        Matriz m = new Matriz(linhas, colunas);
 
+        // m.adiciona(0, 1);// marca uma localização na matriz, que receberá 1
+        // m.adiciona(0, 2);
+        // m.adiciona(0, 3);
+        // m.adiciona(1, 0);
+
+        do{
+            System.out.println("Adiciona uma aresta?\n0 - não\n1 - sim");
+            opcao = sc.nextInt();
+            if(opcao == 1){
+                System.out.println("Digite o valor da linha:");
+                l = sc.nextInt();
+                System.out.println("Digite o valor da linha:");
+                c = sc.nextInt();
+
+                m.adiciona(l, c);
+            }
+            else{
+                break;
+            }
+        }
+        while (opcao != 0);
+
+        System.out.println("---------------- Matriz ----------------");
         System.out.println(m.toString()); // mostra a matriz desenhada
     }
 }
