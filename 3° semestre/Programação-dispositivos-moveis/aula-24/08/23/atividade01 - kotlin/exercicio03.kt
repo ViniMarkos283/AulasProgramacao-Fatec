33eda0c968c2edfe41edb53d938b0941c6a7d3c3
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-08-23 12:48:32 -0300

    123

---
+++ 3° semestre/Programação-dispositivos-moveis/aula-24/08/23/atividade01 - kotlin/exercicio03.kt
@@ -0,0 +1,7 @@
+fun main(){ 
+    println("Digite o primeiro valor: ") 
+    var n1 = readLine().toString().toInt() 
+    println("Digite o segundo valor: ") 
+    var n2 = readLine().toString().toInt() 
+    println("O resultado da multiplicação é: ${n1*n2}") 
+}
---
+++ 3° semestre/Tecnicas-de-programacão-2/aula-24-08-30/desafio Pizzaria/Desafio.js
@@ -0,0 +1,35 @@
+// PROJETO DESAFIO:
+// - Desenvolver em grupo de no max 3 alunos;
+// - Utilizar o Padrão de Projeto Builder, Factory Method e Prototype
+// - Utilizar a linguagem JavaScript (POO)
+
+// ESCOPO:
+// - Um sistema de pizzaria por pizza personalizada:
+
+//   a) Tamanho:
+//     Padrão - 30cm
+//     Brotinho - 20cm
+//     Grande - 35cm
+//     Família - 40cm
+
+//   b) Borda:
+//     Tradicional
+//     Sem Borda
+//     Recheada Catupiry
+//     Recheada Cheddar
+
+//   c) Sabores:
+//     (criar 5 sabores)
+
+//   d) Ingredientes Extra:
+//     (criar 5 opções de ingredientes extra)
+
+//   f) Ingredientes Opcionais de Finalização:
+//     Orégano
+//     Pimenta Calabresa
+//     Pimenta do Reino
+//     Azeitona Verde
+//     Azeitona Preta    
+
+// Entregue os arquivos JS, caso seja necessário entregar algum comentário ou instrução adicionar, adicione um arquivo README explicando. 
+
