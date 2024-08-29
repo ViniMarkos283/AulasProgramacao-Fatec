b2a2d2810304860a6f2aa9bc240a833e42b7e7af
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-08-29 11:09:57 -0300

    add new question (finish him later)

---
+++ 3° semestre/Programação-dispositivos-moveis/Aula-24-08-30/src/main/kotlin/atv03_exe46.kt
@@ -0,0 +1,21 @@
+/*
+46. Faça um programa na Linguagem Kotlin que leia tenha 3 opções:
+ 1 - Fatorial
+ 2 - Quadradro de um número
+ 3 - Volume de uma lata
+ 4 – Sair do Programa
+Utilize os conceitos de funções em Kotlin
+*/
+
+fun main(){
+    val menu = "Escolha uma opção: \n
+    1 - Calcular fatorial \n
+    2 - Calcular quadrado de um valor \n
+    3 - Calcular volume de uma lata \n
+    4 - Sair"
+    
+    
+    
+}
+
+
