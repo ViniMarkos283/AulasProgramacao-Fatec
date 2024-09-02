4479bca58ea8e83a09ce2a032dd31af4296e7e10
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-09-02 15:14:26 -0300

    finish atv04

--- 3° semestre/Programação-dispositivos-moveis/aula-24-09-03/src/main/kotlin/Atv04_exe54.kt
+++ 3° semestre/Programação-dispositivos-moveis/aula-24-09-03/src/main/kotlin/Atv04_exe54.kt
@@ -4,4 +4,21 @@ Armazenar as notas da primeira e da segunda prova em vetores distintos.
 Calcular e escrever a média de cada aluno.
  */
 
-fun main(){}
+fun main(){
+var i = 0
+    var prova1 = Array(5, {0.0})
+    var prova2 = Array(5, {0.0})
+    
+    while(i < prova1.size){
+        println("Digite a nota da primeira prova do ${i+1}° aluno")
+        prova1[i] = readLine().toString().toDouble()
+        println("Digite a nota da segunda prova do ${i+1}° aluno")
+        prova2[i] = readLine().toString().toDouble()
+        i += 1
+    }
+    for(i in 0..4){
+        println("O aluno ${i+1} teve as seguintes notas: prova 1: ${prova1[i]} e prova 2: ${prova2[i]}")
+        println("A media do aluno ${i+1} é de: ${(prova1[i] + prova2[i])/2}")
+    }
+    
+}
--- 3° semestre/Programação-dispositivos-moveis/aula-24-09-03/src/main/kotlin/Atv04_exe57.kt
+++ 3° semestre/Programação-dispositivos-moveis/aula-24-09-03/src/main/kotlin/Atv04_exe57.kt
@@ -6,4 +6,26 @@
 – Após de todos os elementos do vetor A, mostrar os dois vetores
  */
 
-fun main(){}
+fun main(){
+var i = 0
+    var vetorA = Array(5, {0})
+    var vetorB = Array(5, {0})
+    
+    while(i < vetorA.size){
+        println("Digite o ${i+1}° valor: ")
+        vetorA[i] = readLine().toString().toInt()
+        vetorB[i] = fatorial(vetorA[i])
+        i += 1
+    }
+    
+    println(vetorA.joinToString(", "))
+    println(vetorB.joinToString(", "))
+}
+
+fun fatorial(x:Int):Int{
+    if(x < 1){
+        return 1
+    }else{
+        return  x * fatorial(x-1)
+    }
+}
