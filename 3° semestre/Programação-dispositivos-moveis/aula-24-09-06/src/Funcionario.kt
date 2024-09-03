64f6127124f0ee01619e9ca1a39570e168a8213c
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-09-03 11:08:47 -0300

    incomplete question

--- 3° semestre/Programação-dispositivos-moveis/aula-24-09-06/src/Funcionario.kt
+++ 3° semestre/Programação-dispositivos-moveis/aula-24-09-06/src/Funcionario.kt
@@ -1,3 +1,9 @@
-fun Funcionario(){
+class Funcionario(){
+    var nome: String = ""
+    var salarioBruto: Double = 0.0
+    var desconto: Double = 0.0
+}
 
+fun salarioLiquido():double{
+    return salarioBruto - desconto
 }
--- 3° semestre/Programação-dispositivos-moveis/aula-24-09-06/src/Main.kt
+++ 3° semestre/Programação-dispositivos-moveis/aula-24-09-06/src/Main.kt
@@ -1,10 +1,19 @@
 /*
-65. Fazer um programa para ler os dados de um funcionário (nome, salário Bruto e Desconto). 
-Em seguida, mostrar os dados do funcionário (nome e salário líquido). 
-Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada e mostrar novamente os dados do funcionário. 
+65. Fazer um programa para ler os dados de um funcionário (nome, salário Bruto e Desconto).
+Em seguida, mostrar os dados do funcionário (nome e salário líquido).
+Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada e mostrar novamente os dados do funcionário.
 Use a classe projetada abaixo:
 */
 
 fun main(){
-    
+    val func = Funcionario()
+
+    println("nome: ")
+    func.nome = readLine().toString()
+    println("Salario Bruto: ")
+    func.salarioBruto = readLine().toString()
+    println("nome: ")
+    func.desconto = readLine().toString()
+
+    println("Dados do funcionário: ${func.nome}, R$${func.salarioLiquido()}")
 }
