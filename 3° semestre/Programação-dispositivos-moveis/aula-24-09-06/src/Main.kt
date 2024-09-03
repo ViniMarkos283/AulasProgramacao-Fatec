4b474a8b4432e4fdd08aa30d3b6aec1c98c0fa59
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-09-03 10:42:27 -0300

    add new activity

---
+++ 3° semestre/Programação-dispositivos-moveis/aula-24-09-06/src/Main.kt
@@ -0,0 +1,10 @@
+/*
+65. Fazer um programa para ler os dados de um funcionário (nome, salário Bruto e Desconto). 
+Em seguida, mostrar os dados do funcionário (nome e salário líquido). 
+Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada e mostrar novamente os dados do funcionário. 
+Use a classe projetada abaixo:
+*/
+
+fun main(){
+    
+}
