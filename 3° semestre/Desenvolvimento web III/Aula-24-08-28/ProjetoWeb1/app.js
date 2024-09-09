0d279aa5e9b9e5aba25950632ea5f8329183d0c0
Marcos Vinicius <102962949+ViniMarkos283@users.noreply.github.com>
2024-09-09 11:16:25 -0300

    rework scripts

--- 3° semestre/Desenvolvimento web III/Aula-24-08-28/ProjetoWeb1/app.js
+++ 3° semestre/Desenvolvimento web III/Aula-24-08-28/ProjetoWeb1/app.js
@@ -59,17 +59,33 @@ const server = http.createServer((req, res) => {
             // caso as variaveis estejam corretas...
             const imc = peso / (altura * altura);
 
+            // Classificação do IMC:
+            let classificacao;
+            if (imc < 18.5) {
+                classificacao = "Abaixo do peso";
+            } else if (imc < 24.9) {
+                classificacao = "Peso normal";
+            } else if (imc < 29.9) {
+                classificacao = "Sobrepeso";
+            } else if (imc < 39.9) {
+                classificacao = "Obesidade 1";
+            } else {
+                classificacao = "Obesidade grave";
+            }
+
             // classificação do IMC:
             if (imc < 18.5) {
                 // abaixo do peso (cambito)
                 // res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 // res.end("IMC - Abaixo do peso")
-                fs.readFile("abaixodopeso.html", 'utf-8', (err, data) => {
+                fs.readFile("index.html", 'utf-8', (err, data) => {
                     if (err) {
                         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
                         res.end("500 - Erro no servidor")
                     } else {
                         data = data.replace(`{imc}`, imc.toFixed(1)) // vai achar o imc no html e substituir pelo valor
+                        data = data.replace(`{peso}`, classificacao)
+                        console.log(imc)
                         res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' })
                         res.end(data);
                     }
@@ -78,12 +94,13 @@ const server = http.createServer((req, res) => {
                 // peso normal
                 // res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 // res.end("IMC - peso normal")
-                fs.readFile("pesonormal.html", 'utf-8', (err, data) => {
+                fs.readFile("index.html", 'utf-8', (err, data) => {
                     if (err) {
                         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
                         res.end("500 - Erro no servidor")
                     } else {
                         data = data.replace(`{imc}`, imc.toFixed(1)) // vai achar o imc no html e substituir pelo valor
+                        data = data.replace(`{peso}`, classificacao)
                         res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' })
                         res.end(data);
                     }
@@ -93,12 +110,13 @@ const server = http.createServer((req, res) => {
                 // sobre peso
                 // res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 // res.end("IMC - Sobrepeso")
-                fs.readFile("sobrepeso.html", 'utf-8', (err, data) => {
+                fs.readFile("index.html", 'utf-8', (err, data) => {
                     if (err) {
                         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
                         res.end("500 - Erro no servidor")
                     } else {
                         data = data.replace(`{imc}`, imc.toFixed(1)) // vai achar o imc no html e substituir pelo valor
+                        data = data.replace(`{peso}`, classificacao)
                         res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' })
                         res.end(data);
                     }
@@ -107,12 +125,13 @@ const server = http.createServer((req, res) => {
                 // obesidade 1
                 // res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 // res.end("IMC - obesidade 1")
-                fs.readFile("obesidade1.html", 'utf-8', (err, data) => {
+                fs.readFile("index.html", 'utf-8', (err, data) => {
                     if (err) {
                         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
                         res.end("500 - Erro no servidor")
                     } else {
                         data = data.replace(`{imc}`, imc.toFixed(1)) // vai achar o imc no html e substituir pelo valor
+                        data = data.replace(`{peso}`, classificacao)
                         res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' })
                         res.end(data);
                     }
@@ -121,12 +140,13 @@ const server = http.createServer((req, res) => {
                 // obesidade grave (baleia)
                 // res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 // res.end("IMC - Obesidade grave")
-                fs.readFile("obesidadegrave.html", 'utf-8', (err, data) => {
+                fs.readFile("index.html", 'utf-8', (err, data) => {
                     if (err) {
                         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
                         res.end("500 - Erro no servidor")
                     } else {
                         data = data.replace(`{imc}`, imc.toFixed(1)) // vai achar o imc no html e substituir pelo valor
+                        data = data.replace(`{peso}`, classificacao)
                         res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' })
                         res.end(data);
                     }
